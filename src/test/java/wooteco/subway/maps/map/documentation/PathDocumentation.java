7f3760a67f6dc290f90309d821d4985b86fb860d
K.S.KIM <nullable@kakao.com>
2020-08-06 16:38:54 +0900

    test(path): 최단 경로 검색 문서화

--- src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
+++ src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
@@ -1,4 +1,88 @@
 package wooteco.subway.maps.map.documentation;
 
-public class PathDocumentation {
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.web.context.WebApplicationContext;
+
+import wooteco.security.core.TokenResponse;
+import wooteco.subway.common.documentation.Documentation;
+import wooteco.subway.maps.map.application.MapService;
+import wooteco.subway.maps.map.dto.PathResponse;
+import wooteco.subway.maps.map.ui.MapController;
+import wooteco.subway.maps.station.dto.StationResponse;
+
+@WebMvcTest(controllers = {MapController.class})
+public class PathDocumentation extends Documentation {
+    protected TokenResponse tokenResponse;
+
+    @Autowired
+    private MapController mapController;
+
+    @MockBean
+    private MapService mapService;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext,
+            RestDocumentationContextProvider restDocumentationContextProvider) {
+        super.setUp(webApplicationContext, restDocumentationContextProvider);
+        tokenResponse = new TokenResponse("token");
+    }
+
+    @DisplayName("두 역의 최단 경로를 조회한다.")
+    @Test
+    void findPath() {
+        PathResponse pathResponse = new PathResponse(
+                Arrays.asList(new StationResponse(1L, "교대역", LocalDateTime.now(), LocalDateTime.now()),
+                        new StationResponse(2L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                        new StationResponse(3L, "양재역", LocalDateTime.now(), LocalDateTime.now())),
+                4, 3, 1_250);
+        when(mapService.findPath(any(), any(), any())).thenReturn(pathResponse);
+
+        given().log().all().
+                header("Authorization", "Bearer " + tokenResponse.getAccessToken()).
+                accept(MediaType.APPLICATION_JSON_VALUE).
+                when().
+                get("/paths?source=1&target=3&type=DURATION").
+                then().
+                log().all().
+                apply(document("map/paths",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer auth credentials")
+                        ),
+                        requestParameters(
+                                parameterWithName("source").description("출발 아이디"),
+                                parameterWithName("target").description("도착 아이디"),
+                                parameterWithName("type").description("조회 타입")
+                        ),
+                        responseFields(
+                                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로 목록"),
+                                fieldWithPath("stations.[].id").type(JsonFieldType.NUMBER).description("지하철역 아이디"),
+                                fieldWithPath("stations.[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
+                                fieldWithPath("stations.[].createdDate").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("stations.[].modifiedDate").type(JsonFieldType.STRING).description("변경일"),
+                                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("총 거리"),
+                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("총 시간"),
+                                fieldWithPath("fare").type(JsonFieldType.NUMBER).description("요금")))).
+                extract();
+    }
 }
--- src/test/java/wooteco/subway/members/favorite/documentation/FavoriteDocumentation.java
+++ src/test/java/wooteco/subway/members/favorite/documentation/FavoriteDocumentation.java
@@ -1,6 +1,18 @@
 package wooteco.subway.members.favorite.documentation;
 
-import com.google.common.collect.Lists;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,36 +22,22 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.google.common.collect.Lists;
+import wooteco.security.core.TokenResponse;
 import wooteco.subway.common.documentation.Documentation;
 import wooteco.subway.maps.station.dto.StationResponse;
 import wooteco.subway.members.favorite.application.FavoriteService;
 import wooteco.subway.members.favorite.dto.FavoriteResponse;
 import wooteco.subway.members.favorite.ui.FavoriteController;
-import wooteco.security.core.TokenResponse;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 @WebMvcTest(controllers = {FavoriteController.class})
 public class FavoriteDocumentation extends Documentation {
+    protected TokenResponse tokenResponse;
     @Autowired
     private FavoriteController favoriteController;
     @MockBean
     private FavoriteService favoriteService;
-
-    protected TokenResponse tokenResponse;
 
     @BeforeEach
     public void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
@@ -98,10 +96,14 @@ public class FavoriteDocumentation extends Documentation {
                                 fieldWithPath("[].source").type(JsonFieldType.OBJECT).description("출발역"),
                                 fieldWithPath("[].source.id").type(JsonFieldType.NUMBER).description("지하철역 아이디"),
                                 fieldWithPath("[].source.name").type(JsonFieldType.STRING).description("지하철역 이름"),
+                                fieldWithPath("[].source.createdDate").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("[].source.modifiedDate").type(JsonFieldType.STRING).description("변경일"),
                                 fieldWithPath("[].target").type(JsonFieldType.OBJECT).description("도착역"),
                                 fieldWithPath("[].target.id").type(JsonFieldType.NUMBER).description("지하철역 아이디"),
-                                fieldWithPath("[].target.name").type(JsonFieldType.STRING).description("지하철역 이름")))).
-                extract();
+                                fieldWithPath("[].target.name").type(JsonFieldType.STRING).description("지하철역 이름"),
+                                fieldWithPath("[].target.createdDate").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("[].target.modifiedDate").type(JsonFieldType.STRING).description("변경일"))))
+                .extract();
     }
 
     @Test
